8d7edc882c72de4e95957358348adecb0a698611
Elvio Batista <elvio.batista@pebmed.com.br>
2022-05-06 00:30:04 -0300

    verifica fim de jogo e altera placar

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -12,6 +12,8 @@ const game = {
         }
     },
     is_first_player: false,
+    moves: 0,
+    finish: false,
 }
 
 $(document).ready(function() {
@@ -25,7 +27,9 @@ function restart() {
     $(".square").each(function(i, square) {
         square.firstChild.textContent = '-'
     });
-    changePlayer();   
+    changePlayer();
+    game.finish = false;
+    game.moves = 0;   
 }
 
 function getCurrentPlayer() {
@@ -40,11 +44,34 @@ function changePlayer() {
 }
 
 function executeMove(square) {
-    if( square.firstChild.classList.contains(game.players.first.class) ||
+    if( game.finish === true ||
+        square.firstChild.classList.contains(game.players.first.class) ||
         square.firstChild.classList.contains(game.players.second.class)) {
             return;
     }
     square.firstChild.classList.add(getCurrentPlayer().class);
     square.firstChild.textContent = getCurrentPlayer().symbol;
-    changePlayer();
+    
+    const player = getCurrentPlayer();
+    const wins = checkIfWins(player);
+    if (!wins) {
+        changePlayer();
+        return;
+    }
+
+    player.score +=1;
+    game.finish = true;
+    
+    $(`.${player.class}.score`)[0].textContent = player.score;
+}
+
+function checkIfWins (player) {
+    const possibilities = ['diagonal_1', 'diagonal_2', 'up', 'middle', 'bottom', 'left', 'center', 'right']; 
+    let wins = false;   
+    possibilities.forEach(possibility => {
+        if ($(`.${possibility} .${player.class}`).length === 3) {
+            wins = true;
+        }
+    });
+    return wins;
 }
